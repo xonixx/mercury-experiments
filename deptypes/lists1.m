e6894ba46d00d515261472bbc1abe23d96454312
Volodymyr Gubarkov <xonixx@gmail.com>
2011-04-07 15:27:02 +0000

    lists1.m even sized list
    git-svn-id: svn://localhost/mercury@86 670cc495-4fdc-1743-8658-0938d4aec05c

--- deptypes/lists1.m
+++ deptypes/lists1.m
@@ -8,4 +8,26 @@
 
 :- implementation.
 
+:- import_module list.
 
+%:- inst even_list ---> [] ; [ground|even_list]. % XXX
+%:- inst even_list ---> [] ; [ground,ground|even_list]. % YYY
+
+%:- inst even_list ---> []; [ground | bound([ground | even_list])].
+:- inst even_list == bound([]; [ground | bound([ground | even_list])]).
+
+%:- type even_list(T) ---> []; [T,T|even_list(T)].
+
+%:- pred t(even_list(int)).
+:- pred t(list(int)).
+:- mode t(in(even_list)) is semidet.
+t([1,2,3,4]).
+
+main(!IO) :-
+	%L=[1,2,3,4,5,6],
+	L=[1,2,3,4,5,6,7,8,9],
+	(	t(L) ->
+		print("1",!IO)
+	;
+		print("2",!IO)
+	).
