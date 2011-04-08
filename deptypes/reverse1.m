2de5bea2b56bfe82604dc4e1e4e68133e6c616ab
Volodymyr Gubarkov <xonixx@gmail.com>
2011-04-08 10:52:59 +0000

    reverse1 (segfault)
    git-svn-id: svn://localhost/mercury@88 670cc495-4fdc-1743-8658-0938d4aec05c

---
+++ deptypes/reverse1.m
@@ -0,0 +1,27 @@
+:- module reverse1.
+
+:- interface.
+:- import_module io.
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module list.
+
+:- func reverse2(list(int)) = list(int).
+%:- mode reverse2(in) = out.
+:- mode reverse2(in(non_empty_list)) = out(non_empty_list).
+reverse2(A) = reverse2_aux(A, []).
+
+:- func reverse2_aux(list(int), list(int)) = list(int).
+:- mode reverse2_aux(in, in) = out.
+%:- mode reverse2_aux(in(non_empty_list), in) = out(non_empty_list).
+%:- mode reverse2_aux(in, in(non_empty_list)) = out(non_empty_list).
+reverse2_aux([], Acc) = Acc.
+reverse2_aux([A | As], Acc) = Acc.%reverse2_aux(As, [A | Acc]).
+
+main(!IO) :-
+	Y = 1, Ys = [2,3,4,5],
+	print("before",!IO),
+	[X | Xs] = reverse2([Y | Ys]),
+	print({X, Xs}, !IO), nl(!IO).
