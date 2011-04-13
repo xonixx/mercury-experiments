54420879e6eb645ef4d2fcc606bc2f212e804887
Volodymyr Gubarkov <xonixx@gmail.com>
2011-04-13 21:09:55 +0000

    t2 cnt solutions
    git-svn-id: svn://localhost/mercury@102 670cc495-4fdc-1743-8658-0938d4aec05c

--- solver/t2.m
+++ solver/t2.m
@@ -22,5 +22,15 @@ cnt(P) = N :-
 		unsorted_aggregate(P, (pred(_V::in, Acc0:int::in, Acc:int::out) is det :-
 			Acc = Acc0 + 1), 0, N)
 	).
+	
+:- mode qqq(out) is nondet.	
+qqq({/*A,B,*/C,D,E})	:-
+	L = 1..5,
+	%~ perm(L,A),
+	%~ perm(L,B),
+	perm(L,C @ [H|_]), H \=2,
+	perm(L,D),
+	perm(L,E).
 
-main(!IO) :- write_int(cnt(perm(1..5)), !IO).
+%main(!IO) :- write_int(cnt(perm(1..5)), !IO).
+main(!IO) :- write_int(cnt(qqq), !IO).
