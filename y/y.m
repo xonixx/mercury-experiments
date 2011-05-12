2d7a108c7e9e35306a4a240ac7221ef76b5f2c36
Volodymyr Gubarkov <xonixx@gmail.com>
2011-05-12 16:19:59 +0000

    y combinator
    git-svn-id: svn://localhost/mercury@108 670cc495-4fdc-1743-8658-0938d4aec05c

--- y/y.m
+++ y/y.m
@@ -10,6 +10,7 @@
 
 :- import_module int.
 
+/*
 %:- func fix(func((func(A) = A), A) = A) = (func(A) = A).
 :- func fix(func(func(A) = A) = (func(A) = A)) = (func(A) = A).
 fix(F) = F(fix(F)).
@@ -17,6 +18,21 @@ fix(F) = F(fix(F)).
 %:- func fac(int) = int.
 %fac = fix(func(F, N) = (N =< 0 -> 1; N * F(N-1))).
 fac = fix(func(F) = (func(N) = (N =< 0 -> 1; N * F(N-1)))).
+*/
+
+:- type mu(A) ---> roll(unroll :: (func(mu(A)) = A)).
+
+%y(F) = (func(X, A) = F((unroll(X))(X), A))(roll(func(X, A) = F((unroll(X))(X), A))).
+%%y(F) = (func(X) = (func(A) = F(unroll(X)(X))(A)))(roll(func(X) = (func(A) = F(unroll(X)(X))(A)))).
+y(F) = F1(roll(F1)) :- F1 = (func(X) = (func(A) = F(unroll(X)(X))(A))).
+
+y_fac = y(func(F) = (func(N) = (N =< 0 -> 1; N * F(N-1)))).
+%%y_fac(N) = y(func(F) = (func(N =< 0 -> 1; N * F(N-1)))).
+y_fac1(N) = (y_fac)(N).
 
 main(!IO) :-
-	write_int((fac)(10),!IO).
+	%write_int((fac)(10),!IO)
+	write_int((y_fac)(10),!IO), nl(!IO),
+	write_int(y_fac1(10),!IO)
+	
+	.
