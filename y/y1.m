1af9d5dba64515d6f79c820e041f39c2c1ecdaba
Volodymyr Gubarkov <xonixx@gmail.com>
2011-05-13 10:11:40 +0000

    y1
    git-svn-id: svn://localhost/mercury@111 670cc495-4fdc-1743-8658-0938d4aec05c

--- y/y1.m
+++ y/y1.m
@@ -15,12 +15,16 @@
 y(F) = F1(roll(F1)) :- F1 = (func(X) = (func(A) = F(unroll(X)(X))(A))).
 
 y_fac = y(func(F) = (func(N) = (N =< 0 -> 1; N * F(N-1)))).
-
+y_fib = y(func(F) = (func(N) = (N < 2 -> N; F(N-1) + F(N-2)))).
 
 %% let fix f g = (fun x a -> f (x x) a) (fun x a -> f (x x) a) g
 
 %%%fix(F) = F1(F1) :- F1 = (func(X) = (func(A) = F((X)(X))(A))).
 %%%fix_fac =  fix(func(F) = (func(N) = (N =< 0 -> 1; N * F(N-1)))).
 
-main --> write_int((y_fac)(10)). % 3628800
+main --> 
+	write_int((y_fac)(10)), % 3628800
+	nl,
+	write_int((y_fib)(15)).
+	
 %main --> write_int((fix_fac)(10)). % 3628800
