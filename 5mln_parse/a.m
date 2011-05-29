a8eb557a962f972131d8f6e2dd8bfdd8b6a0120f
Volodymyr Gubarkov <xonixx@gmail.com>
2011-05-29 12:45:47 +0000

    parse 5 mln
    git-svn-id: svn://localhost/mercury@114 670cc495-4fdc-1743-8658-0938d4aec05c

--- 5mln_parse/a.m
+++ 5mln_parse/a.m
@@ -8,6 +8,15 @@
 
 :- implementation.
 
+:- import_module int, char, float, list, require, math, string.
+
+number_chars(N, [D|DD]) :- number_chars(N, D, DD).
+number_chars(0, []).
+
+number_chars(N, N, []).
+number_chars(N, N1, [D1|DD]) :- number_chars(N, N1 * 10 + D1, DD).
+
+:- mode integer(out, in, out) is nondet.
 integer(I) -->
         digit(D0),
         digits(D),
@@ -15,44 +24,49 @@ integer(I) -->
         }.
 
 digits([D|T]) -->
-        digit(D), !,
+        digit(D),
         digits(T).
 digits([]) -->
         [].
 
-digit(D) -->
+:- mode digit(out, in, out) is nondet.
+digit(N) -->
         [D],
-        { code_type(D, digit)
+        { char.int_to_digit(N, D)
         }.
 
-
+:- pred float(float::out, list(char)::in, list(char)::out) is nondet.
 float(F) -->
-	(   "-", {Sign = -1}
-	;   "", {Sign = 1}
-	), !,
+	(	
+		['-'], {Sign = -1.0}
+	;   
+		[], {Sign = 1.0}
+	),
 	integer(N),
-	",",
+	[','],
 	integer(D),
-	{F is Sign * (N + D / 10^(ceiling(log10(D))))
+	{F = Sign * (float(N + D) / math.pow(10.0, math.ceiling(log10(float(D)))))
 	}.
 
-
+:- mode sum(out, in, out) is nondet.
 sum(S) -->
-	float(F1), !,
-	" ",
-	(   sum(S1)
-	;   {S1 = 0}
+	float(F1:float),
+	[' '],
+	(   sum(S1:float)
+	;   {S1 = 0.0}
 	),
-	{ S is F1 + S1}.
+	{ S = F1 + S1}.
 
+:- mode sum(in, out, in, out) is nondet.
 sum(S, Total) -->
-	float(F1), !,
-	" ",
-	{ S1 is S + F1},
+	float(F1),
+	[' '],
+	{ S1 = S + F1},
 	sum(S1, Total).
 sum(Total, Total) -->
 	[].
 
+/*
 go :-
 	read_file_to_codes('numbers_large.txt',Codes,[]),
 	writeln('Processing...'),
@@ -62,3 +76,33 @@ go :-
 go1 :-
 	phrase_from_file(sum(0, S),'numbers_large.txt', [buffer_size(16384)]),
 	writeln(S).
+*/
+
+get_chars_from_current_stream(Chars) -->
+	read_file(Result),
+	{	Result = ok(Chars)
+	;	
+		Result = error(_,Error),
+		error(error_message(Error))
+	}.
+
+main(!IO) :-
+	see("D:/numbers_large.txt", Result, !IO),
+	(	Result = ok,
+		get_chars_from_current_stream(Chars, !IO),
+		seen(!IO),
+
+		promise_equivalent_solutions [S1]
+		(	sum(0.0, S, Chars, [])
+		->	
+			S1 = S
+		;	
+			error("WTF")
+		),
+		write(S1, !IO),
+		nl(!IO)
+	;	
+		Result = error(Error),
+		write_string(error_message(Error), !IO)	
+	).
+	
